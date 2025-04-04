ce54dffc698876eff36604b0f54d330c62100438
akshat k <inquiry.akshatkotpalliwar@gmail.com>
2025-04-04 11:44:07 +0530

    rm redis && add config.service

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,18 +1,22 @@
-// src/db/database.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Deployment } from '../deployment/deployment.entity';
+import { ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.PG_HOST || 'localhost',
-      port: process.env.PG_PORT ? parseInt(process.env.PG_PORT, 10) : 5432,
-      username: process.env.PG_USERNAME || 'postgres',
-      password: process.env.PG_PASSWORD || 'password',
-      database: process.env.PG_DATABASE || 'flexhost',
-      entities: [Deployment],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST', 'localhost'),
+        port: parseInt(configService.get('PG_PORT'), 10) || 5432,
+        username: configService.get('PG_USERNAME', 'postgres'),
+        password: configService.get('PG_PASSWORD', 'password'),
+        database: configService.get('PG_DATABASE', 'flexhost'),
+        entities: [Deployment],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
