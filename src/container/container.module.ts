3b90807fa446fbfb3268b57076b7ce443b7304f5
akshat k <inquiry.akshatkotpalliwar@gmail.com>
2025-04-04 17:40:17 +0530

    Changes in frontend init

--- src/container/container.module.ts
+++ src/container/container.module.ts
@@ -4,10 +4,10 @@ import { ContainerService } from './container.service';
 import { ContainerController } from './container.controller';
 import { SubdomainModule } from '../subdomain/subdomain.module';
 import { DeploymentModule } from '../deployment/deployment.module';
-
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [DeploymentModule, SubdomainModule],
-  providers: [ContainerService],
+  providers: [ContainerService, ConfigService],
   controllers: [ContainerController],
 })
 export class ContainerModule {}
--- src/htmx/htmx.controller.ts
+++ src/htmx/htmx.controller.ts
@@ -2,11 +2,18 @@
 import { Controller, Post, Get, Query, Body, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { HtmxService } from './htmx.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('htmx')
 export class HtmxController {
-  constructor(private readonly htmxService: HtmxService) {}
-
+  public nickname: string;
+  constructor(
+	  private readonly htmxService: HtmxService ,
+	  private readonly configService: ConfigService
+	 
+  ) {
+	  this.nickname = this.configService.get<string>('GH_USERNAME', 'IntegerAlex');
+  }
   @Post()
   async runContainer(@Body() body: any, @Res() res: Response) {
     const { userName, buildCommand, runCommand, repoLink, entryPoint } = body;
@@ -60,5 +67,32 @@ export class HtmxController {
       console.error('Error fetching subscription:', error);
       res.send('<p>Error fetching subscription</p>');
     }
+  }
+
+  @Get('v1/profile')
+  async getProfile(@Res() res: Response) {
+	return res.json({nickname: this.nickname});
+  }
+  @Get('v1/repositories')
+  async getRepositories(@Res() res: Response) {
+	 fetch(`https://api.github.com/${this.nickname}/repos`, {
+		method: 'GET'})
+	.then((response) => response.json())
+    	.then((data: any) => {
+   	   const repositories = data.map(
+       		 (repo: { name: string; html_url: string }) => ({
+          	name: repo.name,
+          	url: repo.html_url,
+        	})
+      	);
+      	res.send({
+        repositories: repositories,
+        avatar_url: data[0]?.owner?.avatar_url,
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching repositories:', error);
+      res.status(500).send('Error fetching repositories');
+    });
   }
 }
